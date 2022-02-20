ab053e0c4ac355baf93e007e366df1a27e0176d0
eemdeeks <prince9088@gmail.com>
2022-02-20 17:57:55 +0900

    wss 연결
    frontend와 backend 를 wss통해서 연결

--- 줌/zoom/src/public/js/app.js
+++ 줌/zoom/src/public/js/app.js
@@ -1,1 +1,22 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//서버와 연결된 경우(브라우져 들어가자마자 발생)
+socket.addEventListener("open",()=>{
+    console.log("conndected to server o");
+});
+
+//서버로부터 메세지를 받았을 경우
+socket.addEventListener("message", (message) => { 
+    console.log("just got this: ", message, "from the server");
+});
+
+//연결이 끊겼을 경우
+socket.addEventListener("close", () =>{
+    console.log("Disconnected from Server x")
+});
+
+
+//10초 뒤에 메시지 보내기
+setTimeout(() => {
+    socket.send("hello from the browser!");
+},10000);
--- 줌/zoom/src/server.js
+++ 줌/zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,7 +9,20 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req,res) => res.redirect("/"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
+const server = http.createServer(app); //http 서버
 
-app.listen(3000, handleListen);
+const wss = new WebSocket.Server({ server }); //ws 서버
+
+wss.on("connection",(socket)=>{
+    console.log("conndected to Browser ");
+    socket.on("close", () => console.log("Disconnected from the Browser x"));
+    socket.on("message",(message) => {
+        console.log(message.toString('utf-8'));
+    });
+    socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
