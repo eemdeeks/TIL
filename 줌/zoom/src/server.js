e1be86030d83b25c995c8518318fe1b00184aa2e
eemdeeks <prince9088@gmail.com>
2022-03-02 23:06:47 +0900

    nickname, msg

--- 줌/zoom/src/server.js
+++ 줌/zoom/src/server.js
@@ -16,14 +16,28 @@ const httpServer = http.createServer(app); //http 서버
 const wsServer = SocketIo(httpServer); //ws 서버
 
 wsServer.on("connection", socket => {
+
+    socket["nickname"] = "Anon";
     socket.onAny((event)=>{
         console.log(`Socket Event:${event}`);
     });
     socket.on("enter_room", (roomName, done)=> {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room)=> socket.to(room).emit("bye",socket.nickname));
+    });
+
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+        done();
+    });
+
+    socket.on("nickname", (nickname) => (socket["nickname"]= nickname));
+    
 });
 
 
